d9f0767f04d7ed4ac37b979195d1fc4f94150687
Healer1205 <my-name@chromium.org>
2020-01-11 02:53:01 -0500

    first react-redux project commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+
+import store from "./js/store/index";
+import { addArticle } from "./js/actions/index";
+window.store = store;
+window.addArticle = addArticle;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
